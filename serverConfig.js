6e778d35b6ffa413702e83a035023da1855190ed
wangpb <wangpb@Ctrip.com>
2017-05-27 19:30:07 +0800

    add serverConfig.js

---
+++ serverConfig.js
@@ -0,0 +1,10 @@
+
+exports.devConfig = {
+	CDN_URL: 'http://localhost:3000',
+	DB_URL: 'mongodb://localhost:27017/yourclass'
+}
+
+exports.prdConfig = {
+	CDN_URL: 'https://yourclass.herokuapp.com',
+	DB_URL: 'mongodb://heroku_n3krjlv2:7r1lu1a9dlhki6v1lei9sfpi8q@ds155811.mlab.com:55811/heroku_n3krjlv2'
+}
